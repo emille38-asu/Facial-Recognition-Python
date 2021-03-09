088f46aabad67d13eae823ae9b5f0c20d5a6efab
“[Edward <[emille38@asu.edu]>
2021-03-08 21:23:19 -0600

    Added openCV grayscale

--- main.py
+++ main.py
@@ -7,3 +7,16 @@ cascPath = sys.argv[2]
 
 # Create haar code
 faceCascade = cv2.CascadeClassifier(cascPath)
+
+# Read image
+image = cv2.imread(imagePath)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# Detect faces in the image
+faces = faceCascade.detectMultiScale(
+    gray,
+    scaleFactor = 1.1,
+    minNeighbors = 5,
+    minSize = (30, 30),
+    flags = cv2.cv.CV_HAAR_SCALE_IMAGE
+)
